1f8d54753980fbd9277a915927969fbeba9e8ad7
Rosana Rezende <rezende_rosana@hotmail.com>
2023-04-11 22:01:58 -0300

    atualiza prop drilling e composição

--- REACT/Semana-07/02.propDrilling_contextAPI/src/components/organisms/Composition/Composition.jsx
+++ REACT/Semana-07/02.propDrilling_contextAPI/src/components/organisms/Composition/Composition.jsx
@@ -1,11 +1,35 @@
+import { WelcomeCard, WelcomeCard2, WelcomeCard3 } from '../../molecules'
+import { Input, WelcomeMessage } from '../../atoms'
+
+
 export function Composition() {
+  const user = {
+    id: 'abc',
+    name: 'João',
+    age: 20,
+  }
+  const placeholder = 'Digite seu nome'
+  const label = 'João'
+
   return (
     <section>
       <h2>Composition</h2>
 
+      {/* Prop drilling */}
+      <WelcomeCard user={user} placeholder={placeholder} />
+
+
       <h3>Podemos usar PROPS para enviar o componente</h3>
+      <WelcomeCard2
+        mensagemAuxiliar={<WelcomeMessage name={user.name} />}
+        inputAuxiliar={<Input placeholder={placeholder} label={label} />}
+      />
 
       <h3>Podemos usar CHILDREN para enviar o componente</h3>
+      <WelcomeCard3>
+        <WelcomeMessage name={user.name} />
+        <Input placeholder={placeholder} label={label} />
+      </WelcomeCard3>
     </section>
   );
 }
---
+++ REACT/Semana-07/02.propDrilling_contextAPI/src/contexts/user.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const user = {
+    id: 1,
+    name: "Fulano de Tal",
+    age: 36,
+  }
+
+  return (
+    <UserContext.Provider value={{ user }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
